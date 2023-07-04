66aa2829aa8be9080ceebebd0db19f294cd286e1
김진건 <87012465+jingeon27@users.noreply.github.com>
2023-07-04 21:21:49 +0900

    fix : code style 수정

--- src/apis/auth/auth.controller.ts
+++ src/apis/auth/auth.controller.ts
@@ -1,24 +1,14 @@
-import { Controller, Get, Req, UseGuards } from "@nestjs/common";
-import { AuthGuard } from "@nestjs/passport";
-import { AuthService } from "./auth.service";
+import { Controller, Get, Req, Res, UseGuards } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { AuthService } from './auth.service';
 
 @Controller('auth')
 export class AuthController {
+  constructor(private readonly authService: AuthService) {}
 
-     constructor(
-          private authService: AuthService
-     ) {}
-
-     @UseGuards(AuthGuard('google'))
-     @Get('google')
-     async googleAuth() {
-
-     }
-
-     @Get('google/redirect')
-     @UseGuards(AuthGuard('google'))
-     async googleAuthRedirect(@Req() req: any): Promise<void> {
-       this.authService.
-     }
-
+  @Get('/login/:social')
+  @UseGuards(DynamicAuthGuard)
+  loginOAuth(@Req() req: Request, @Res() res: Response) {
+    return this.authService.
+  }
 }
