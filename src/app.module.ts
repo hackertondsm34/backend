2c44d5e8540d81f237cfa313c6aac106db9f981a
이하성 <tedsoftj1123@gmail.com>
2023-07-05 02:00:41 +0900

    feat: naver oauth

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,11 @@ import { AuthModule } from './apis/auth/auth.module';
 import { GoogleStrategy } from './apis/auth/strategies/google.strategy';
 import { UserModule } from './apis/user/user.module';
 import { KakaoStrategy } from './apis/auth/strategies/kakao.strategy';
+import { NaverStrategy } from './apis/auth/strategies/naver.strategy';
 
 @Module({
   imports: [ConfigModule.forRoot(), AuthModule, UserModule],
   controllers: [],
-  providers: [GoogleStrategy, KakaoStrategy],
+  providers: [GoogleStrategy, KakaoStrategy, NaverStrategy],
 })
 export class AppModule {}
